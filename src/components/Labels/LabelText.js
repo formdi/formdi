131e12b85bcc2f05cfcf3139e7863ce074a2bac7
Vicente0709 <78982050+Vicente0709@users.noreply.github.com>
2024-12-04 08:54:32 -0500

    Delete All

--- src/components/Labels/LabelText.js
+++
@@ -1,5 +0,0 @@
-const LabelText = ({ text }) => {
-  return <p className="form-info-text" dangerouslySetInnerHTML={{ __html: text }} />;
-};
-
-export default LabelText;
--- src/components/Labels/LabelTitle.js
+++
@@ -1,5 +0,0 @@
-const LabelTitle = ({ text, disabled }) => {
-  return <h2 className={`form-title ${disabled ? "disabled" : ""}`}>• {text}</h2>;
-};
-
-export default LabelTitle;
